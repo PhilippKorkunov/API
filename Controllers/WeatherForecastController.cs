d5ed8b3956433710b802c0c1d336a2736400b2aa
PhilippKorkunov <filippkorkunov.1920@gmail.com>
2023-11-30 02:55:21 +0300

    del

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,10 +13,7 @@ namespace API.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+
 
         [HttpGet(Name = "GetWeatherForecast")]
         public WeatherForecast Get(int index)
